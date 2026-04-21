ef89fadbb269a36a9cb8e7087d5956decb85d464
Yudbay1809 <yudbay1809@github.com>
2026-04-22 00:40:57 +0700

    fix: railway build permission by removing tracked node_modules

--- dist/commands.d.ts
+++
@@ -1,4 +0,0 @@
-import { Telegraf } from 'telegraf';
-import { MyContext } from './types';
-export declare function registerCommands(bot: Telegraf<MyContext>): void;
-//# sourceMappingURL=commands.d.ts.map
